2a5f0bd6c1d1f46229d0fbaea6822e042c7f0376
Raghunathssj <ssjrnath1@gmail.com>
2018-03-20 14:35:20 +0530

    Added a functionality to check pattern of account number

--- library/src/main/java/com/bank/Account.java
+++ library/src/main/java/com/bank/Account.java
@@ -1,19 +1,27 @@
 package com.bank;
 
-public class Account {
-    public static final int MinimumBalance = 1000;
-    private final String accountNumber;
-    private final int balance;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-    public Account(String acccountNumber, int balance) throws MinimumBalanceException {
-        this.accountNumber = acccountNumber;
-        if(balance < MinimumBalance){
+public class Account {
+    private static final int MINIMUM_BALANCE = 1000;
+    private final String accountNumber;
+    private final float balance;
+    private static final Pattern PATTERN = Pattern.compile("\\d{4}[-]\\d{4}");
+
+    public Account(String accountNumber, float balance) throws MinimumBalanceException, InvalidAccountNumberException {
+        Matcher matcher = PATTERN.matcher(accountNumber);
+        if (!matcher.matches()){
+            throw new InvalidAccountNumberException();
+        }
+        this.accountNumber = accountNumber;
+        if (balance < MINIMUM_BALANCE) {
             throw new MinimumBalanceException();
         }
         this.balance = balance;
     }
 
-    public int getBalance() {
+    public float getBalance() {
         return balance;
     }
 
--- library/src/test/java/AccountTest.java
+++ library/src/test/java/AccountTest.java
@@ -1,4 +1,5 @@
 import com.bank.Account;
+import com.bank.InvalidAccountNumberException;
 import com.bank.MinimumBalanceException;
 import org.junit.Before;
 import org.junit.Test;
@@ -11,22 +12,27 @@ public class AccountTest {
     private Account account;
 
     @Before
-    public void setUp() throws MinimumBalanceException {
-        account = new Account("1234", 1000);
+    public void setUp() throws MinimumBalanceException, InvalidAccountNumberException {
+        account = new Account("1234-1234", 1000);
     }
 
     @Test
     public void getBalance() {
-        assertThat(account.getBalance(), is(1000));
+        assertThat(account.getBalance(), is((float) 1000));
     }
 
     @Test
     public void getAccountNumber() {
-        assertThat(account.getAccountNumber(),is("1234"));
+        assertThat(account.getAccountNumber(),is("1234-1234"));
     }
 
     @Test(expected = MinimumBalanceException.class)
-    public void checkMinimumBalance() throws MinimumBalanceException {
-        new Account("1234",200);
+    public void checkMinimumBalance() throws MinimumBalanceException, InvalidAccountNumberException {
+        new Account("1234-1234",200);
+    }
+
+    @Test(expected = InvalidAccountNumberException.class)
+    public void checkAccountNumber() throws InvalidAccountNumberException, MinimumBalanceException {
+        new Account("1234",1000);
     }
 }
