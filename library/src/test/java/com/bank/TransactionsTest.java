bf55a9f005159c56162b15e849c7ec43c0da8f3e
Raghunathssj <ssjrnath1@gmail.com>
2018-03-22 21:12:52 +0530

    Added a test to check both debit and credit transactions are storing or not.

--- library/src/test/java/com/bank/TransactionsTest.java
+++ library/src/test/java/com/bank/TransactionsTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import java.util.Date;
 
 import static org.hamcrest.core.IsCollectionContaining.hasItem;
+import static org.hamcrest.core.IsCollectionContaining.hasItems;
 import static org.junit.Assert.assertThat;
 
 public class TransactionsTest {
@@ -20,5 +21,13 @@ public class TransactionsTest {
         Transactions transactions = new Transactions();
         transactions.credit(1000,"from_me");
         assertThat(transactions.transactions,hasItem(new CreditTransaction(new Date(),1000,"from_me")));
+    }
+
+    @Test
+    public void mustRecordBothCreditAndDebitTransactions() {
+        Transactions transactions = new Transactions();
+        transactions.debit(2000,"to_me");
+        transactions.credit(3000,"from_someone");
+        assertThat(transactions.transactions,hasItems(new DebitTransaction(new Date(),2000,"to_me"),new CreditTransaction(new Date(),3000,"from_someone")));
     }
 }
